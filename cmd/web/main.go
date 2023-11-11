73fe0295710fb5a80be6b21a1cb162cec58977e7
Chahine-tech <chahine.benlahcen@gmail.com>
2023-11-11 16:52:50 +0100

    feat: add a basic structure

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -18,7 +18,7 @@ func main() {
 
 	// handler function #1 - returns the index.html template, with film data
 	h1 := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl := template.Must(template.ParseFiles("cmd/web/templates/index.html"))
 		films := map[string][]Film{
 			"Films": {
 				{Title: "The Godfather", Director: "Francis Ford Coppola"},
@@ -36,7 +36,7 @@ func main() {
 		director := r.PostFormValue("director")
 		// htmlStr := fmt.Sprintf("<li class='list-group-item bg-primary text-white'>%s - %s</li>", title, director)
 		// tmpl, _ := template.New("t").Parse(htmlStr)
-		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl := template.Must(template.ParseFiles("cmd/web/templates/index.html"))
 		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
 	}
 
